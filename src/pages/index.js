593fc1cd3508f052f93e2ddb5a4bf5fda0dcdb1b
MeSka338 <halk49114@gmail.com>
2023-10-03 13:38:49 +0300

    Refactor index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
-import Head from "next/head";
-import { Inter } from "next/font/google";
 import HomePage from "../components/pages/HomePage";
 import Layout from "../components/common/Layout";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
